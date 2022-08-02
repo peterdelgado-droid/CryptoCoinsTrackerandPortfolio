17d16c96955226836c99a4a981e8b1033bcd55a5
Peter Delgado <peterrafaeldelgado@gmail.com>
2022-08-02 13:01:51 +0200

    added themes

---
+++ SwiftCryptoTracker/Extensions/Color.swift
@@ -0,0 +1,27 @@
+//
+//  Color.swift
+//  SwiftCryptoTracker
+//
+//  Created by Peter Delgado on 2/8/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+
+ static let theme = ColorTheme()
+
+
+}
+
+struct ColorTheme{
+
+	let accent = Color("AccentColor")
+	let background = Color("BackGroundColor")
+	let green = Color("GreenColor")
+	let red = Color("RedColor")
+	let secondaryText = Color("SecondaryTextColor")
+
+
+}
