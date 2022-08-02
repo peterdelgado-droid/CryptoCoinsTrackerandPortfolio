395f381e64e4384ebe1182f5bd7c30bb87ce977b
Peter Delgado <peterrafaeldelgado@gmail.com>
2022-08-02 11:41:01 +0200

    Initial Commit

---
+++ SwiftCryptoTracker/SwiftCryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoTrackerApp.swift
+//  SwiftCryptoTracker
+//
+//  Created by Peter Delgado on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
