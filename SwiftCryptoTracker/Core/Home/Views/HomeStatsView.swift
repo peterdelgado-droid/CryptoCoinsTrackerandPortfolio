566f576924ad6c44a22c56c95d0e03d45e86e60d
Peter Delgado <peterrafaeldelgado@gmail.com>
2023-09-12 12:01:51 +0200

    formatting

--- SwiftCryptoTracker/Core/Home/Views/HomeStatsView.swift
+++ SwiftCryptoTracker/Core/Home/Views/HomeStatsView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct HomeStatsView: View {
 
-
-
 	@EnvironmentObject private var vm: HomeViewModel
-
 	@Binding var showPortofolio: Bool
 
 	var body: some View {
@@ -20,7 +17,6 @@ struct HomeStatsView: View {
 			ForEach(vm.statistics) { stat in
 				StatisticView(stat: stat)
 					.frame(width: UIScreen.main.bounds.width / 3)
-
 			}
 		}
 		.frame(width: UIScreen.main.bounds.width, alignment: showPortofolio ? .trailing : .leading)
