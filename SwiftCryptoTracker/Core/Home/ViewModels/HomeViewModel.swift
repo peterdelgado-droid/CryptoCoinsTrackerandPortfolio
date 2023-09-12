2183b7a7017040156baf2002a04a0b0813e69cf0
Peter Delgado <peterrafaeldelgado@gmail.com>
2023-09-12 12:06:56 +0200

    formatting

--- SwiftCryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftCryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -20,8 +20,6 @@ class HomeViewModel: ObservableObject {
    //view model allCoins
 	@Published var VMallCoins: [CoinModel] = []
 	@Published var portfolioCoins: [CoinModel] = []
-
-
 	@Published var searchText: String = ""
 
 	private let dataService = CoinDataService()
@@ -40,16 +38,11 @@ class HomeViewModel: ObservableObject {
 
 				guard let data = marketDataModel else {
 					return stats
-
 				}
-
-				let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange:
-												data.marketCapChangePercentage24HUsd)
+				let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange:data.marketCapChangePercentage24HUsd)
 				let volume = StatisticModel(title: "24h Volume", value: data.volume, percentageChange: 5 )
 				let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance, percentageChange: 8)
 				let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange:0 )
-
-
 				stats.append(contentsOf:[
 					marketCap,
 					volume,
@@ -63,37 +56,28 @@ class HomeViewModel: ObservableObject {
 				self?.statistics = returnedStats
 			}
 			.store(in: &cancellables)
-
-
 		dataService.$allCoins
 			.sink { [weak self] (returnedCoins) in
 				self?.VMallCoins = returnedCoins
 			}
 			.store(in: &cancellables)
-		
-		
 		$searchText.combineLatest(dataService.$allCoins)
 			.map{ (text, startingCoins) -> [CoinModel] in
 				
 				guard !text.isEmpty else{
 					return startingCoins
 				}
-				
 				let lowerCase = text.lowercased()
-				
 				return startingCoins.filter { (coin) -> Bool in
 					return coin.name.lowercased().contains(lowerCase) ||
 					coin.symbol.lowercased().contains(lowerCase) ||
 					coin.id.lowercased().contains(lowerCase)
 				}
-				
-				
+
 			}
-		
 			.sink { [weak self] (returnedCoins) in
 				self?.VMallCoins = returnedCoins
 			}
 			.store(in: &cancellables)
+		}
 	}
-
-}
