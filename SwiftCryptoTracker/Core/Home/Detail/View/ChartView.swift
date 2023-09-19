dcf15a3332a722e1c5d4138c2e466075b62a8368
Peter Delgado <peterrafaeldelgado@gmail.com>
2023-09-19 12:06:19 +0200

    updates

--- SwiftCryptoTracker/Core/Home/Detail/View/ChartView.swift
+++ SwiftCryptoTracker/Core/Home/Detail/View/ChartView.swift
@@ -8,13 +8,72 @@
 import SwiftUI
 
 struct ChartView: View {
+
+	let data: [Double]
+	let maxY: Double
+	let minY: Double
+	let lineColor: Color
+
+
+	init(coin: CoinModel){
+		data = coin.sparklineIn7D?.price ?? []
+		maxY = data.max() ?? 0
+		minY = data.min() ?? 0
+
+		let priceChange = (data.last ?? 0) - (data.first ?? 0)
+		lineColor = priceChange > 0 ? Color.theme.green: Color.theme.red
+	}
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack{
+			chartView
+				.frame(height: 200)
+				.background(
+
+				)
+		}
     }
 }
 
 struct ChartView_Previews: PreviewProvider {
     static var previews: some View {
-        ChartView()
+		ChartView(coin: dev.coin)
     }
+}
+
+extension ChartView {
+
+	private var chartView: some View {
+		GeometryReader { geometry in
+			Path { path in
+				for index in data.indices {
+					let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+
+					let yAxis = maxY - minY
+
+					let yPosition = (1 - CGFloat((data[index] - minY) / yAxis)) *
+					geometry.size.height
+
+					if index == 0 {
+						path.move(to: CGPoint(x: xPosition, y: yPosition))
+					}
+					path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+
+				}
+			}
+			.stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+		}
+	}
+	private var chartBackground: some View {
+		VStack{
+			Divider()
+			Spacer()
+			Divider()
+			Spacer()
+			Divider()
+
+		}
+	}
+
+
 }
