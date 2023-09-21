cf1ff659ad50bb160ecb98eaf8f36c0e3f1a7cd7
Peter Delgado <peterrafaeldelgado@gmail.com>
2023-09-21 13:48:05 +0200

    updates

--- SwiftCryptoTracker/Core/Home/Views/HomeView.swift
+++ SwiftCryptoTracker/Core/Home/Views/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
 	@EnvironmentObject private var vm: HomeViewModel
 	@State private var showPortfolio: Bool = false
 	@State private var showPorfolioView: Bool = false// new sheet
-
+	@State private var showSettingsView: Bool = false
 	@State private var selectedCoin: CoinModel? = nil
 	@State private var showDetailView: Bool = false
 
@@ -47,6 +47,9 @@ struct HomeView: View {
 				}
 				Spacer(minLength: 0)
 			}
+			.sheet(isPresented: $showSettingsView, content: {
+				SettingsView()
+			})
 		}
 		.background(
 			NavigationLink(
@@ -79,6 +82,8 @@ extension HomeView {
 				.onTapGesture {
 					if showPortfolio{
 						showPorfolioView.toggle()
+					} else {
+						showSettingsView.toggle()
 					}
 				}
 				.background(
